6c2bc6e901065440e44debeaad7eb59a03e218ee
Asaed Reyes Medina <asaed.reyes@kubofinanciero.com>
2022-12-14 02:02:30 -0600

    [update] card pokemon info

--- src/pages/Pokedex/components/CardPokemonInfo.tsx
+++ src/pages/Pokedex/components/CardPokemonInfo.tsx
@@ -1,11 +1,45 @@
-import { IonCard } from "@ionic/react";
 import React from "react";
+import {
+  IonCard,
+  IonCardContent,
+  IonCardTitle,
+  IonCol,
+  IonContent,
+  IonImg,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonRow,
+} from "@ionic/react";
 
-const CardPokemonInfo = () => {
+const CardPokemonInfo = ({ name, data }: any) => {
   return (
     <>
       <IonCard>
-        
+        <IonCardContent>
+          <IonRow>
+            <IonCol size="8">
+              <IonCardTitle className="namePokemonCard">
+                {name.toUpperCase()}
+              </IonCardTitle>
+            </IonCol>
+            <IonCol size="4">
+              <IonImg
+                className="imgPokemonCard"
+                src={data.sprites.front_default}
+                alt={name}
+              ></IonImg>
+            </IonCol>
+            <IonCol size="12">
+              <IonList>
+                <IonItem>
+                  <IonLabel slot="start">Experiancia base:</IonLabel>
+                  <IonLabel slot="end">{data.base_experience}</IonLabel>
+                </IonItem>
+              </IonList>
+            </IonCol>
+          </IonRow>
+        </IonCardContent>
       </IonCard>
     </>
   );
